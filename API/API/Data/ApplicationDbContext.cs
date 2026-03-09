2be22410f5e9937c09b2e96288504cf7e7ec6ac5
Simão Joaquim <scsj70@gmail.com>
2026-03-09 10:55:24 +0000

    All tables created

--- API/API/Data/ApplicationDbContext.cs
+++ API/API/Data/ApplicationDbContext.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+
+
+        /* tables of our database */
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public DbSet<MyUser> MyUsers { get; set; }
+
+        public DbSet<Photography> Photos { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
